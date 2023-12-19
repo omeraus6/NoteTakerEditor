30b2a1e0e066880d54884783e209022286340049
omeraus6 <omeraus6@yahoo.com>
2023-12-18 22:14:37 -0800

    connect html pages with server.js

--- routes/HTMLRoute.js
+++ routes/HTMLRoute.js
@@ -1,21 +1,9 @@
 const htm = require('express').Router();
 const path = require('path');
-const { readFromFile } = require('../helpers/fsUtils');
-const termData = require('../db/db.json');
-const fs = require('fs');
-const { json } = require('express');
-
-
-
 
 
 htm.get('/notes', (req, res) =>{
   res.sendFile(path.join(__dirname, '../public/notes.html'));
-  //readFromFile('./db/db.json').then((data) => res.json(JSON.stringify(data)));
-  //res.send(path.join(__dirname, '../db/db.json'));
-  //res.json(JSON.stringify(data));
-  //res.json(termData.slice(1));
-
 });
 
 htm.get('*', (req, res) =>
